7a9951259fe908cc12fdeab3eeed025bd007d7d2
OzarkOC <155337375+OzarkOC@users.noreply.github.com>
2024-01-01 18:13:55 -0600

    try again: changed string

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'Anika Parham';
+  const name = 'Anika D Parham';
   greetUser(name);
 });
